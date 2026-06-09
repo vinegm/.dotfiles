8119a4edecb3b072589d11e8d82a195e9a0c09ce
vine <117782568+vinegm@users.noreply.github.com>
2026-06-09 15:06:21 -0300

    better cd/ls/cat with fallbacks

--- config/scripts/.local/bin/fzf-previewer.sh
+++ config/scripts/.local/bin/fzf-previewer.sh
@@ -2,10 +2,23 @@
 
 file="$1"
 
+lsCmd=("ls" "-oAh" "--color=always")
+catCmd=("cat")
+
+if command -v eza >/dev/null 2>&1; then
+    lsCmd=("eza" "-lah" "--color=always")
+fi
+
+if command -v bat >/dev/null 2>&1; then
+    catCmd=("bat" "--style=numbers" "--color=always")
+elif command -v batcat >/dev/null 2>&1; then
+    catCmd=("batcat" "--style=numbers" "--color=always")
+fi
+
 if [[ -d "$file" ]]; then
-    ls -oAh --color=always -- "$file"
+    "${lsCmd[@]}" -- "$file"
 elif [[ -f "$file" ]]; then
-    bat --style=numbers --color=always -- "$file"
+    "${catCmd[@]}" -- "$file"
 else
     exit 1
 fi
