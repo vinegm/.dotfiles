3bb27844616040ad3a5ae9fd958870180a7c2fc2
vine <117782568+vinegm@users.noreply.github.com>
2026-06-09 15:56:15 -0300

    Overengineered fzf-previewer

--- config/scripts/.local/bin/fzf-previewer.sh
+++ config/scripts/.local/bin/fzf-previewer.sh
@@ -1,24 +1,71 @@
 #!/usr/bin/env bash
 
+previewDirectory() {
+    if command -v eza >/dev/null 2>&1; then
+        eza -lah --color=always -- "$1"
+        return
+    fi
+
+    ls -oAh --color=always -- "$1"
+}
+
+previewText() {
+    batArgs=(
+        --style=numbers
+        --color=always
+        --pager=never
+    )
+
+    if command -v bat >/dev/null 2>&1; then
+        bat \
+            "${batArgs[@]}" \
+            -- "$1"
+        return
+    fi
+
+    if command -v batcat >/dev/null 2>&1; then
+        batcat \
+            "${batArgs[@]}" \
+            -- "$1"
+        return
+    fi
+
+    cat -- "$1"
+}
+
+previewImage() {
+    if ! command -v kitty >/dev/null 2>&1; then
+        echo "Kitty is required for image previews."
+        return
+    fi
+
+    kitty +kitten icat \
+        --clear \
+        --transfer-mode=memory \
+        --stdin=no \
+        --place "${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0" \
+        -- "$1"
+}
+
+previewFile() {
+    local mime
+    mime=$(file --mime-type -Lb "$1")
+
+    case "$mime" in
+    image/*)
+        previewImage "$1"
+        ;;
+    *)
+        previewText "$1"
+        ;;
+    esac
+}
+
 file="$1"
-
-lsCmd=("ls" "-oAh" "--color=always")
-catCmd=("cat")
-
-if command -v eza >/dev/null 2>&1; then
-    lsCmd=("eza" "-lah" "--color=always")
-fi
-
-if command -v bat >/dev/null 2>&1; then
-    catCmd=("bat" "--style=numbers" "--color=always")
-elif command -v batcat >/dev/null 2>&1; then
-    catCmd=("batcat" "--style=numbers" "--color=always")
-fi
-
 if [[ -d "$file" ]]; then
-    "${lsCmd[@]}" -- "$file"
+    previewDirectory "$file"
 elif [[ -f "$file" ]]; then
-    "${catCmd[@]}" -- "$file"
+    previewFile "$file"
 else
     exit 1
 fi
